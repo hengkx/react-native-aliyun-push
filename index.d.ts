33342d365ad3d4682157fbfb17a57f73649c8128
Tom Xu <ycxzhkx@gmail.com>
2022-08-04 12:07:14 +0800

    v 0.1.5

--- index.d.ts
+++ index.d.ts
@@ -20,33 +20,96 @@ export interface PushMessage {
    */
   extras: Record<string, any>;
 }
+/**
+ * 目标类型
+ */
+export enum TargetType {
+  /**
+   * 本设备
+   */
+  CurrentDevice = 1,
+  /**
+   * 本设备绑定的账号
+   */
+  CurrentAccount = 2,
+  /**
+   * 别名
+   */
+  Alias = 3,
+}
 
 type Callback = (message: PushMessage) => void;
 
 export default class AliyunPuth {
   static addListener(callback: Callback): void;
   static removeListener(callback: Callback): void;
+
   /**
    * 同步角标数到阿里云服务端 (仅ios支持)
    */
   static syncBadgeNum(num: number): void;
+
   /**
    * 获取初始消息
    */
   static getInitialMessage(): Promise<PushMessage>;
+
   /**
    * 获取设备Id
    */
   static getDeviceId(): Promise<string>;
+
   /**
    * 绑定账号
    * @param account 账号
    */
   static bindAccount(account: string): Promise<string>;
+
   /**
    * 解绑账号
    */
   static unbindAccount(): Promise<string>;
+
+  /**
+   * 绑定TAG
+   * @param target 目标类型
+   * @param tags 标签（数组输入）
+   * @param alias 别名，仅当target=3时生效
+   */
+  static bindTag(target: TargetType, tags: string[], alias: string[]): Promise<string>;
+
+  /**
+   * 解绑TAG
+   * @param target 目标类型
+   * @param tags 标签（数组输入）
+   * @param alias 别名，仅当target=3时生效
+   */
+  static unbindTag(target: TargetType, tags: string[], alias: string[]): Promise<string>;
+
+  /**
+   * 查询目标绑定的标签，当前仅支持查询设备标签。
+   * @param target 目标类型 1：本设备
+   */
+  static listTag(target: TargetType): Promise<string[]>;
+
+  /**
+   * 添加别名
+   * 单个设备最多添加128个别名，同一个别名最多可被添加到128个设备。
+   * 别名支持的最大长度为128字节。
+   */
+  static addAlias(alias: string): Promise<string>;
+
+  /**
+   * 删除别名
+   * @param alias alias = null or alias.length = 0 时，删除设备全部别名
+   */
+  static removeAlias(alias?: string): Promise<string>;
+
+  /**
+   * 查询别名
+   */
+  static listAliases(): Promise<string[]>;
+
   /**
    * 设置桌面图标角标数字 (ios支持，android支持绝大部分手机)
    */
